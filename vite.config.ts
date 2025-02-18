b6c5c73422dda3248d9bda02a61dcf4c4b05f8e1
YieldRay <yieldray@outlook.com>
2025-02-18 17:06:57 +0800

    switch tsup to vite build

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { URL, fileURLToPath } from "node:url";
+
+export default defineConfig({
+    build: {
+        outDir: "dist",
+        minify: true,
+        sourcemap: false,
+        lib: {
+            entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+            formats: ["cjs", "es"],
+            fileName: (format) => {
+                switch (format) {
+                    case "es":
+                        return "index.mjs";
+                    default:
+                        return "index.js";
+                }
+            },
+        },
+        rollupOptions: {
+            external: [/^node:/],
+        },
+    },
+    plugins: [dts()],
+    clearScreen: false,
+});
